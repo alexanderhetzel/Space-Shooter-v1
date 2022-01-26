4aedbe7294c283334e5ded9fa5277350f11fc71a
alexanderhetzel <alexander.hetzel@htl-wels.at>
2022-01-26 23:16:42 +0100

    FINISHED

--- core/src/com/mygdx/game/SpaceShooter.java
+++ core/src/com/mygdx/game/SpaceShooter.java
@@ -3,6 +3,8 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -13,15 +15,19 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
 
+import java.io.File;
 import java.util.Iterator;
 
 public class SpaceShooter extends ApplicationAdapter {
     // Größe der Welt
+
     private final int WIDTH = 854;
     private final int HEIGHT = 480;
-    private final int DEFENSESHIP_SPEED = 300;
-    private final int ENEMYSHIP_SPEED = 300;
+    private int DEFENSESHIP_SPEED = 400; //nicht final weil variabel
+    private int ENEMYSHIP_SPEED = 300;
     private final int LASERSHOT_SPEED = 500;
+    private int ENEMYLASERSHOT_SPEED = 500;
+
 
 
     private final int GAMESPEED = 1000;
@@ -31,6 +37,8 @@ public class SpaceShooter extends ApplicationAdapter {
     private Texture defenseShipImage;
     private Texture enemyShipImage;
     private Texture backgroundPlanetsImage; //Planeten im Hintergrund
+    //private Array<Texture> asteroidImages;
+    private Texture asteroidImage;
     private Texture laserImage;
     private Texture enemyLaserImage;
     private Texture gameOverScreen;
@@ -39,17 +47,18 @@ public class SpaceShooter extends ApplicationAdapter {
     private int collisionCounter = 0;
     private Texture heartImage;
 
-    //private Music rainMusic;
-    //private Sound dropSound;
+    private Music backgroundMusic;
+    private Sound crashSound;
     Texture background1, background2;
     float yMax, yCoordBg1, yCoordBg2;
-    final int BACKGROUND_MOVE_SPEED = 75; // pixels per second. Put your value here.
+    int BACKGROUND_MOVE_SPEED = 75; // pixels per second. Put your value here.
 
     //Camera
     private OrthographicCamera camera;
 
     // Spielfiguren
     private Rectangle defenseship;
+    private Array<Rectangle> asteroids;
     private Array<Rectangle> enemyShips;
     private Array<Rectangle> laserShots;
     private Array<Rectangle> enemyLaserShots;
@@ -60,9 +69,15 @@ public class SpaceShooter extends ApplicationAdapter {
     // Zeitstempel des letzten Regentropfens
     private long lastDropTime;
 
+    private long lastAsteroidTime;
+    private boolean asteroidflag = true; //für dynamische Hinzufügung der Asteroiden, d.h. sobald alle asteroidimages importiert ist flag flase
+
     @Override
     public void create() {
         enemyShips = new Array<>();
+
+        asteroids = new Array<>();
+        //asteroidImages = new Array<>();
         laserShots = new Array<>();
         enemyLaserShots = new Array<>();
         hearts = new Array<>(3);
@@ -72,9 +87,31 @@ public class SpaceShooter extends ApplicationAdapter {
         batch = new SpriteBatch();
 
 
+        //Lade Assets in Array
+        /*for (int i = 1; asteroidflag; i++) {
+            asteroidImages.add(new Texture("asteroid"+i+".png"));
+
+            if (new File("asteroid"+(i+1)+".png").exists()){
+                asteroidflag=false;
+            }
+        }
+
+         */
+
+        /*asteroidImages.add(new Texture("asteroid1.png"));
+        asteroidImages.add(new Texture("asteroid2.png"));
+        asteroidImages.add(new Texture("asteroid3.png"));
+        asteroidImages.add(new Texture("asteroid4.png"));
+        asteroidImages.add(new Texture("asteroid5.png"));
+
+         */
+
+
+
         // Lade Assets
         defenseShipImage = new Texture("DefenseShip64x64-Dark.png");
         enemyShipImage = new Texture("EnemyShip64x64.png");
+        asteroidImage = new Texture("asteroid1.png");
         laserImage = new Texture("laser48x11.png");
         enemyLaserImage = new Texture("laserenemy48x11.png");
         heartImage = new Texture("heart.png");
@@ -89,15 +126,16 @@ public class SpaceShooter extends ApplicationAdapter {
         yCoordBg1 = yMax * (1);
         yCoordBg2 = 0;
 
-        /*rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-        dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("LHS_RLD10.mp3"));
+        crashSound = Gdx.audio.newSound(Gdx.files.internal("EnemyHit.mp3"));
 
         // Musik abspielen + loopen
-        rainMusic.setVolume(0.1f);
-        rainMusic.setLooping(true);
-        rainMusic.play();
+        backgroundMusic.setVolume(0.1f);
+        backgroundMusic.setLooping(true);
+        backgroundMusic.play();
 
-         */
+
+
 
         // Kamera erstellen
         camera = new OrthographicCamera();
@@ -110,8 +148,7 @@ public class SpaceShooter extends ApplicationAdapter {
         defenseship.x = 20;
         defenseship.y = HEIGHT / 2 - defenseship.height / 2;
 
-        spawnEnemyShip();
-
+        spawnAsteroids();
 
     }
 
@@ -146,7 +183,18 @@ public class SpaceShooter extends ApplicationAdapter {
         laserShots.add(laserShot);
 
     }
-    spawn
+
+    private void spawnAsteroids() {
+        Rectangle asteroid = new Rectangle();
+        asteroid.width = 48;
+        asteroid.height = 48;
+        asteroid.x = WIDTH;
+        asteroid.y = MathUtils.random(0, HEIGHT - asteroid.height);
+
+        asteroids.add(asteroid);
+        lastAsteroidTime = TimeUtils.millis();
+
+    }
 
     private void spawnEnemyLaser(Rectangle enemyship) {
         Rectangle enemyLaserShot = new Rectangle();
@@ -177,15 +225,33 @@ public class SpaceShooter extends ApplicationAdapter {
          */
 
             // Tastenabfragen
+            if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+                DEFENSESHIP_SPEED = 600;
+            else DEFENSESHIP_SPEED = 400;
+
             if (Gdx.input.isKeyPressed(Input.Keys.A))
-                defenseship.x -= DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
+                defenseship.x -= DEFENSESHIP_SPEED/1.5 * Gdx.graphics.getDeltaTime();
             if (Gdx.input.isKeyPressed(Input.Keys.D))
-                defenseship.x += DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
+                defenseship.x += DEFENSESHIP_SPEED/1.5 * Gdx.graphics.getDeltaTime();
             if (Gdx.input.isKeyPressed(Input.Keys.W))
                 defenseship.y += DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
             if (Gdx.input.isKeyPressed(Input.Keys.S))
                 defenseship.y -= DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
+
             if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) spawnLaser();
+
+            /*if (Gdx.input.isKeyPressed(Input.Keys.A) && Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+                defenseship.x -= DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
+            if (Gdx.input.isKeyPressed(Input.Keys.D) && Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+                defenseship.x += DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
+            if (Gdx.input.isKeyPressed(Input.Keys.W) && Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+                defenseship.y += DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
+            if (Gdx.input.isKeyPressed(Input.Keys.S) && Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT))
+                defenseship.y -= DEFENSESHIP_SPEED * Gdx.graphics.getDeltaTime();
+
+             */
+
+
 
 
             // Spielfigur darf den Bildschirm nicht verlassen
@@ -199,6 +265,8 @@ public class SpaceShooter extends ApplicationAdapter {
             if (TimeUtils.millis() - lastDropTime > GAMESPEED) //spawnEnemyShip();
                 spawnEnemyLaser(spawnEnemyShip());
 
+            if (TimeUtils.millis() - lastAsteroidTime - 1000 > GAMESPEED) //spawnEnemyShip();
+                spawnAsteroids();
 
             // enemy fliegen lassen lassen
             Iterator<Rectangle> iterEnemyShip = enemyShips.iterator();
@@ -219,13 +287,35 @@ public class SpaceShooter extends ApplicationAdapter {
                 }
             }
 
+
+            // asteroid fliegen lassen lassen
+            Iterator<Rectangle> iterAsteroid = asteroids.iterator();
+            while (iterAsteroid.hasNext()) {
+                Rectangle asteroid = iterAsteroid.next();
+                asteroid.x -= (ENEMYSHIP_SPEED - 100) * Gdx.graphics.getDeltaTime();
+                //asteroid.y -= ( MathUtils.random(-500,500)) * Gdx.graphics.getDeltaTime();
+
+
+                // EnemyShip außerhalb Bildschirmbereich
+                if (asteroid.x < 0 - asteroid.width)
+                    iterAsteroid.remove();
+
+                // Kollision Defense- und EnemyShip
+                if (defenseship.overlaps(asteroid)) {
+                    collisionCounter++;
+                    iterAsteroid.remove();
+                    //renderGameOverScreen();
+                    //dropSound.play();
+                }
+            }
+
             //enemylaser fliegen lassen
             Iterator<Rectangle> iterEnemyLaser = enemyLaserShots.iterator();
 
             while (iterEnemyLaser.hasNext()) {
 
                 Rectangle enemylasershot = iterEnemyLaser.next();
-                enemylasershot.x -= LASERSHOT_SPEED * Gdx.graphics.getDeltaTime();
+                enemylasershot.x -= ENEMYLASERSHOT_SPEED * Gdx.graphics.getDeltaTime();
 
                 // EnemyLaser außerhalb Bildschirmbereich
                 if (enemylasershot.x < 0)
@@ -258,15 +348,20 @@ public class SpaceShooter extends ApplicationAdapter {
                     if (lasershot.overlaps(enemyShip)) {
                         iterLaser.remove();
                         iterEnemyShip2.remove();
+                        ENEMYSHIP_SPEED = ENEMYSHIP_SPEED + 5;
+                        ENEMYLASERSHOT_SPEED = ENEMYLASERSHOT_SPEED + 5;
+                        BACKGROUND_MOVE_SPEED = BACKGROUND_MOVE_SPEED +1;
                         score++;
-                        //dropSound.play();
+                        crashSound.play(0.1f);
                     }
                 }
 
             }
 
 
-            yCoordBg1 -= BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+            //yCoordBg1 -= (DEFENSESHIP_SPEED+600) * Gdx.graphics.getDeltaTime(); //variable speed
+
+            yCoordBg1 -= BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime(); //constant speed
             yCoordBg2 = yCoordBg1 - yMax;  // We move the background, not the camera
             if (yCoordBg1 <= 0) {
                 yCoordBg1 = yMax * (1);
@@ -279,7 +374,7 @@ public class SpaceShooter extends ApplicationAdapter {
             batch.draw(background1, yCoordBg1, 0);
             batch.draw(background2, yCoordBg2, 0);
 
-            batch.draw(backgroundPlanetsImage,200,200);
+            batch.draw(backgroundPlanetsImage, 200, 200);
 
             font.getData().setScale(1f);
             font.draw(batch, String.valueOf(score), WIDTH - 80, HEIGHT - 20);
@@ -296,6 +391,17 @@ public class SpaceShooter extends ApplicationAdapter {
             if (!(collisionCounter > 0)) batch.draw(heartImage, WIDTH - 220, HEIGHT - 55);
 
 
+            //Asteroiden rendern
+
+            /*for (Rectangle asteroid : asteroids) {
+
+                batch.draw(asteroidImages.get(MathUtils.random(0, asteroids.size)), asteroid.x, asteroid.y,asteroid.);
+            }
+             */
+            for (Rectangle asteroid : asteroids) {
+
+                batch.draw(asteroidImage, asteroid.x, asteroid.y);
+            }
 
 
             // Regentropfen rendern
@@ -320,6 +426,9 @@ public class SpaceShooter extends ApplicationAdapter {
             enemyShips.clear();
             laserShots.clear();
             enemyLaserShots.clear();
+            ENEMYSHIP_SPEED = 400;
+            ENEMYLASERSHOT_SPEED = 500;
+            BACKGROUND_MOVE_SPEED = 75;
             score = 0;
             batch.setProjectionMatrix(camera.combined);
             batch.begin();
